8e91fb1dbb79ae1ccfd48e946c19f11e6ff46b1d
Geara0 <adrezz.26.5@gmiail.com>
2023-08-20 16:41:50 +0500

    fix system ui

--- lib/layouts/main/main_layout.dart
+++ lib/layouts/main/main_layout.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:news_demo/icomoon_icons.dart';
 import 'package:news_demo/router/router.dart';
 
@@ -18,53 +19,56 @@ class _MainLayoutState extends State<MainLayout>
 
   @override
   Widget build(BuildContext context) {
-    return AutoTabsRouter.tabBar(
-      physics: const NeverScrollableScrollPhysics(),
-      builder: (context, child, _) {
-        final tabsRouter = context.tabsRouter;
-        return Scaffold(
-          body: child,
-          bottomNavigationBar: Padding(
-            padding:
-                EdgeInsets.only(bottom: MediaQuery.paddingOf(context).bottom),
-            child: SizedBox(
-              height: 55,
-              child: TabBar(
-                onTap: tabsRouter.setActiveIndex,
-                controller: _tabController,
-                tabs: [
-                  Tab(
-                    icon: const Icon(Icomoon.home),
-                    text: 'mainLayout.tabs.main'.tr(),
-                    iconMargin: EdgeInsets.zero,
-                  ),
-                  Tab(
-                    icon: const Icon(Icomoon.calendar),
-                    text: 'mainLayout.tabs.events'.tr(),
-                    iconMargin: EdgeInsets.zero,
-                  ),
-                  Tab(
-                    icon: const Icon(Icomoon.chat),
-                    text: 'mainLayout.tabs.chat'.tr(),
-                    iconMargin: EdgeInsets.zero,
-                  ),
-                  Tab(
-                    icon: const Icon(Icons.circle),
-                    text: 'mainLayout.tabs.profile'.tr(),
-                    iconMargin: EdgeInsets.zero,
-                  ),
-                ],
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: AutoTabsRouter.tabBar(
+        physics: const NeverScrollableScrollPhysics(),
+        builder: (context, child, _) {
+          final tabsRouter = context.tabsRouter;
+          return Scaffold(
+            body: child,
+            bottomNavigationBar: Padding(
+              padding:
+                  EdgeInsets.only(bottom: MediaQuery.paddingOf(context).bottom),
+              child: SizedBox(
+                height: 55,
+                child: TabBar(
+                  onTap: tabsRouter.setActiveIndex,
+                  controller: _tabController,
+                  tabs: [
+                    Tab(
+                      icon: const Icon(Icomoon.home),
+                      text: 'mainLayout.tabs.main'.tr(),
+                      iconMargin: EdgeInsets.zero,
+                    ),
+                    Tab(
+                      icon: const Icon(Icons.brush_outlined),
+                      text: 'mainLayout.tabs.canvas'.tr(),
+                      iconMargin: EdgeInsets.zero,
+                    ),
+                    Tab(
+                      icon: const Icon(Icomoon.bell),
+                      text: 'mainLayout.tabs.notifications'.tr(),
+                      iconMargin: EdgeInsets.zero,
+                    ),
+                    Tab(
+                      icon: const Icon(Icons.circle),
+                      text: 'mainLayout.tabs.profile'.tr(),
+                      iconMargin: EdgeInsets.zero,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      },
-      routes: const [
-        MainPageRoute(),
-        PlaceholderPageRoute(),
-        PlaceholderPageRoute(),
-        ProfilePageRoute(),
-      ],
+          );
+        },
+        routes: const [
+          MainPageRoute(),
+          CanvasPageRoute(),
+          NotificationsPageRoute(),
+          ProfilePageRoute(),
+        ],
+      ),
     );
   }
 }
